247329a5a68d315a6a3e941c4edff13c2f4b9ea5
Anna Dorota Pawlukiewicz <ap223uu@student.lnu.se>
2024-01-10 21:31:41 +0100

    printing received messages

--- src/js/modules/chat.js
+++ src/js/modules/chat.js
@@ -1,7 +1,9 @@
 import { AppWindow } from './appWindow'
-import * as ws from './webSocketClient.js'
 
 export class Chat extends AppWindow {
+  #url = 'wss://courselab.lnu.se/message-app/socket'
+  #websocket
+
   constructor () {
     super()
     console.log('chat constructor')
@@ -12,21 +14,17 @@ export class Chat extends AppWindow {
 
     this.#buildChat()
 
-    this.#sendMessage()
+    this.#openSocket()
   }
 
   disconnectedCallback () {
     console.log('chat removed.')
-  }
-
-  #sendMessage () {
-    ws.connect('wss://courselab.lnu.se/message-app/socket')
-
-    ws.send('ANNA WAS HERE')
+    this.#closeSocket()
   }
 
   #buildChat () {
     const chatContainer = document.createElement('div')
+    chatContainer.setAttribute('id', 'chatContainer')
     chatContainer.classList.add('column')
 
     const sendRow = document.createElement('div')
@@ -42,10 +40,68 @@ export class Chat extends AppWindow {
     sendButton.setAttribute('type', 'submit')
     sendButton.setAttribute('value', 'Send')
     sendButton.addEventListener('click', () => {
-      ws.send(sendTextbox.value)
+      this.#sendMessage(sendTextbox.value)
     })
     sendRow.appendChild(sendButton)
 
     this.appBox.appendChild(chatContainer)
+  }
+
+  #messageReceived (data) {
+    console.log(data)
+
+    const username = data.username
+    const messageContents = data.data
+
+    // building the elements
+    const messageRow = document.createElement('div')
+    messageRow.classList.add('row', 'chat-message-row')
+    chatContainer.appendChild(messageRow)
+
+    const messageUsername = document.createElement('p')
+    messageUsername.textContent = username
+    messageRow.appendChild(messageUsername)
+
+    const messageText = document.createElement('p')
+    messageText.classList.add('chat-message-text')
+    messageText.textContent = messageContents
+    messageRow.appendChild(messageText)
+
+    document.getElementById('chatContainer').appendChild(messageRow)
+  }
+
+  #openSocket () {
+    this.#websocket = new WebSocket(this.#url)
+
+    this.#websocket.onopen = () => {
+        console.log('web socket is open: ', this.#websocket)
+    }
+
+    this.#websocket.onmessage = (event) => {
+        console.log('web socket received message: ' + event.data)
+        console.log(event)
+        this.#messageReceived(JSON.parse(event.data))
+    }
+
+    this.#websocket.onclose = () => {
+        console.log('web socket is closed')
+        // todo: print connection is closed message
+    }
+  }
+
+  #closeSocket () {
+    this.#websocket.send('Client closing connection by intention.')
+    this.#websocket.close()
+  }
+
+  #sendMessage (messageText) {
+    // todo: put key/username etc. into json
+
+    if (!this.#websocket || this.#websocket.readyState !== 1) {
+      console.log('The websocket is not open.')
+    } else {
+      this.#websocket.send(messageText)
+      console.log('sending message: ' + messageText)
+    }
   }
 }
